769f3199820e75983deaa3ff4bb46d975eb2768d
eagle013005-gla <eagle013005@gmail.com>
2026-05-24 10:23:23 +0800

    update

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 class Main{
     public static void main(String[] args) {
         System.out.println("Hello World2");//codespace
-        System.out.println(x: "Hello World3");//vscode local pc
+        System.out.println("Hello World3");//vscode local pc
+        System.out.println("Hello World4");
     }   
 }

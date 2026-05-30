33970e274e6e081f9166bdec513461bfff88d8ae
eagle013005-gla <eagle013005@gmail.com>
2026-05-30 21:00:10 +0800

    update

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,10 +4,6 @@ class Main{
         System.out.println("Hello World3");//vscode local pc
         System.out.println("Hello World4");
         System.out.println("Hello World");
-    }   
-}
-
-    public static void main(String[] args) {
         // Primitives (except long and double)
         char h = 'H';
         int three = 3;
@@ -20,6 +16,14 @@ class Main{
         String output = h + "" + three + one + ten + " w" + (ten - ten) + "rld " + two + " " + t;
 
         System.out.println(output);
+
     }
+}
+
+
+
+
+
+    
 
 
---
+++ largestnumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class largestnumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter three numbers: ");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        if (a == b && b == c) {
+            System.out.println("All numbers are equal");
+        } else if (a >= b && a >= c) {
+            System.out.println("The largest number is " + a);
+        } else if (b >= a && b >= c) {
+            System.out.println("The largest number is " + b);
+        } else {
+            System.out.println("The largest number is " + c);
+        }
+        scanner.close();
+    }
+}

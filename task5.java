b211fc44bbf295445b954c6d3c5ac239b1d36e46
eagle013005-gla <eagle013005@gmail.com>
2026-06-05 12:43:58 +0000

    update

---
+++ task5.java
@@ -0,0 +1,25 @@
+//Finding the largest number
+
+import java.util.Scanner;
+
+public class task5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter three numbers: ");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        if (a == b && b == c) {
+            System.out.println("All numbers are equal");
+        } else if (a >= b && a >= c) {
+            System.out.println("The largest number is " + a);
+        } else if (b >= a && b >= c) {
+            System.out.println("The largest number is " + b);
+        } else {
+            System.out.println("The largest number is " + c);
+        }
+        scanner.close();
+    }
+}

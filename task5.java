693ef8c732f53310275889a8059293ab559d0b16
eagle013005-gla <eagle013005@gmail.com>
2026-06-20 07:30:35 +0000

    task5

--- task5.java
+++ task5.java
@@ -6,9 +6,11 @@ public class task5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter three numbers: ");
+        System.out.print("Enter first number: ");
         int a = scanner.nextInt();
+        System.out.print("Enter second number: ");
         int b = scanner.nextInt();
+        System.out.print("Enter third number: ");
         int c = scanner.nextInt();
 
         if (a == b && b == c) {
